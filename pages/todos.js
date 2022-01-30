6e000479986bf2ffa48344855626fd5492ac1398
ynonp <ynonperek@gmail.com>
2022-01-30 10:35:31 +0200

    initial commit

---
+++ pages/todos.js
@@ -0,0 +1,48 @@
+import styles from '../styles/Home.module.css'
+import { useState } from 'react';
+import { listTodos } from '../lib/todos';
+import { useRouter } from 'next/router';
+
+export async function getServerSideProps(context) {
+  console.log(context);
+  return {
+    props: {
+      todos: listTodos(),
+    },
+  }
+}
+
+export default function Todos(props) {
+  const { todos } = props;
+  const [text, setText] = useState('');
+  const router = useRouter();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    fetch('/api/todos', {
+      method: 'POST',
+      body: JSON.stringify(text),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then(() => {
+       router.replace(router.asPath);
+    });
+  }
+
+  return (
+    <div className={styles.container}>
+      <p>My Todo App</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
+        <input type="submit" value="Submit" />
+      </form>
+      <ul>
+        {todos.map((todo, idx) => (
+          <li key={idx}>{todo}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
